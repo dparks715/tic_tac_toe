e2e888568341a3d22dc55fe9ba43e1d2c7937f5a
Daniel Parks <dparks715@yahoo.com>
2017-03-19 03:48:33 -0400

    Creating new classes depending on the players the user selects

--- app.rb
+++ app.rb
@@ -14,12 +14,40 @@ enable :sessions
 
 	end
 
-	post '/game' do
+	post '/select_players' do
 		session[:player1] = params[:player1]
 		session[:player2] = params[:player2]
-		erb :main_board, :locals => {player1: session[:player1], player2: session[:player2], board: session[:board]}
+
+		case session[:player1]
+		when 'Human'
+			session[:player1] = Human.new('X')
+		when 'Easy'
+			session[:player1] = Sequential.new('X')
+		when 'Medium'
+			session[:player1] = RandomAI.new('X')
+		when 'Impossible!'
+			session[:player1] = UnbeatableAI.new('X')
+		else
+			redirect '/'
+		end
+
+		case session[:player2]
+		when 'Human'
+			session[:player2] = Human.new('O')
+		when 'Easy'
+			session[:player2] = Sequential.new('O')
+		when 'Medium'
+			session[:player2] = RandomAI.new('O')
+		when 'Impossible!'
+			session[:player2] = UnbeatableAI.new('O')
+		else
+			redirect '/'
+		end
+		
 
 	end
+
+	#erb :main_board, :locals => {player1: session[:player1], player2: session[:player2], board: session[:board]}
 
 
 # end
